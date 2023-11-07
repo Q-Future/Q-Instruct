b0bc454db28d2ef327c5e7cd47bb2fd93a6149a7
Teo (Timothy) Wu Haoning <38696372+teowu@users.noreply.github.com>
2023-11-07 12:57:37 +0800

    Create get_mm_projectors.sh

---
+++ scripts/llava_v1.5/get_mm_projectors.sh
@@ -0,0 +1,5 @@
+mkdir -p LLaVA/checkpoints/llava-v1.5-13b-pretrain/
+mkdir -p LLaVA/checkpoints/llava-v1.5-7b-pretrain/
+
+wget -P LLaVA/checkpoints/llava-v1.5-13b-pretrain/ https://huggingface.co/liuhaotian/llava-v1.5-mlp2x-336px-pretrain-vicuna-13b-v1.5/resolve/main/mm_projector.bin
+wget -P LLaVA/checkpoints/llava-v1.5-7b-pretrain/ https://huggingface.co/liuhaotian/llava-v1.5-mlp2x-336px-pretrain-vicuna-7b-v1.5/resolve/main/mm_projector.bin

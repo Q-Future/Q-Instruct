9260a15105823d332f159d8d39957899de7a0c53
Teo (Timothy) Wu Haoning <38696372+teowu@users.noreply.github.com>
2023-11-07 12:58:17 +0800

    Update get_mm_projectors.sh

--- scripts/llava_v1.5/get_mm_projectors.sh
+++ scripts/llava_v1.5/get_mm_projectors.sh
@@ -1,5 +1,5 @@
-mkdir -p LLaVA/checkpoints/llava-v1.5-13b-pretrain/
-mkdir -p LLaVA/checkpoints/llava-v1.5-7b-pretrain/
+mkdir -p ./checkpoints/llava-v1.5-13b-pretrain/
+mkdir -p ./checkpoints/llava-v1.5-7b-pretrain/
 
-wget -P LLaVA/checkpoints/llava-v1.5-13b-pretrain/ https://huggingface.co/liuhaotian/llava-v1.5-mlp2x-336px-pretrain-vicuna-13b-v1.5/resolve/main/mm_projector.bin
-wget -P LLaVA/checkpoints/llava-v1.5-7b-pretrain/ https://huggingface.co/liuhaotian/llava-v1.5-mlp2x-336px-pretrain-vicuna-7b-v1.5/resolve/main/mm_projector.bin
+wget -P ./checkpoints/llava-v1.5-13b-pretrain/ https://huggingface.co/liuhaotian/llava-v1.5-mlp2x-336px-pretrain-vicuna-13b-v1.5/resolve/main/mm_projector.bin
+wget -P ./checkpoints/llava-v1.5-7b-pretrain/ https://huggingface.co/liuhaotian/llava-v1.5-mlp2x-336px-pretrain-vicuna-7b-v1.5/resolve/main/mm_projector.bin
